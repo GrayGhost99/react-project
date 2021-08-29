6cbb4ba7f68113dcb999988be9d68447b6de672e
GrayGhost99 <grayghost992003.mm@gmail.com>
2021-08-29 18:56:33 -0400

    Added grid for FAQ page, various fixes

--- src/FAQ.js
+++ src/FAQ.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Flippy, { FrontSide, BackSide } from 'react-flippy';
+import { Grid } from "@material-ui/core"
+
 
 class FAQ extends Component {
     render() {
@@ -6,15 +9,100 @@ class FAQ extends Component {
             <div>
                 <h2>FAQ</h2>
                 <p>Be sure to check out the FAQ before contacting us, your question may have already been answered!</p>
-                <ol className= "centerList">
-                    <li>Are all guitarists listed on your artists page endorsed by Picks Of Destiny?</li>
-                    (//Make animated answer box)
-                    <li>What is your minimum order for guitar picks?</li>
-                    (//Make animated answer box)
-                    <li>Does your ink wear off?</li>
-                    (//Make animated answer box)
-                </ol>
+                
+            <Grid container>
+                <Grid item xs={12} sm={3}>
+                <Flippy
+                    flipOnHover={false} 
+                    flipOnClick={true}
+                    flipDirection="horizontal" 
+                    ref={(r) => this.flippy = r}
+                    style={{ width: '250px', height: '150px' }}>
+
+                    <FrontSide
+                        style={{backgroundColor: '#B5B6EA',}}>
+                        Question:
+                        <hr/>
+                        Are all guitarist listed on your Artists page endorsed by Picks Of Destiny?
+                        </FrontSide>
+                        <BackSide
+                           style={{ backgroundColor: '#989AF0'}}>
+                            Answer:
+                            <hr/>
+                            All of the artists listed have some form of affiliation with Picks Of Destiny.
+                        </BackSide>
+                </Flippy>
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                <Flippy
+                    flipOnHover={false} 
+                    flipOnClick={true}
+                    flipDirection="horizontal" 
+                    ref={(r) => this.flippy = r}
+                    style={{ width: '250px', height: '150px' }}>
+
+                    <FrontSide
+                        style={{backgroundColor: '#B5B6EA',}}>
+                        Question:
+                        <hr/>
+                        What is your minimum order?
+                        </FrontSide>
+                        <BackSide
+                           style={{ backgroundColor: '#989AF0'}}>
+                            Answer:
+                            <hr/>
+                            Our minimum order is 100 picks. A quantity of 1 is 100 picks.
+                        </BackSide>
+                </Flippy>
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                <Flippy
+                    flipOnHover={false} 
+                    flipOnClick={true}
+                    flipDirection="horizontal" 
+                    ref={(r) => this.flippy = r}
+                    style={{ width: '250px', height: '150px' }}>
+
+                    <FrontSide
+                        style={{backgroundColor: '#B5B6EA',}}>
+                        Question:
+                        <hr/>
+                        Does your ink wear off?
+                        </FrontSide>
+                        <BackSide
+                           style={{ backgroundColor: '#989AF0'}}>
+                            Answer:
+                            <hr/>
+                            Our supplier uses state-of-the-art ink to ensure it will not wear off.
+                        </BackSide>
+                </Flippy>
+                </Grid>
+                <Grid item xs={12} sm={3}>
+                <Flippy
+                    flipOnHover={false} 
+                    flipOnClick={true}
+                    flipDirection="horizontal" 
+                    ref={(r) => this.flippy = r}
+                    style={{ width: '250px', height: '150px' }}>
+
+                    <FrontSide
+                        style={{backgroundColor: '#B5B6EA',}}>
+                        Question:
+                        <hr/>
+                        How so I know your picks are good quality?
+                        </FrontSide>
+                        <BackSide
+                           style={{ backgroundColor: '#989AF0'}}>
+                            Answer:
+                            <hr/>
+                            Check out our Reviews section to see what our customers are saying.
+                        </BackSide>
+                </Flippy>
+                </Grid>
+                </Grid>
             </div>
+            
+            
         );
     }
 }
--- src/Reviews.js
+++ src/Reviews.js
@@ -7,6 +7,9 @@ class Reviews extends Component {
                 <h2>Reviews</h2>
                 <p>“Perfect! Thank you so much they’re great.” - J. Stauffer</p>
                 <p>“Rec'd the picks!They are fantastic, Thank you very much.” - J. Lach</p>
+                <p>"I could not have asked for better service, A+" - B. Smith</p>
+                <p>"I have always had a great experience from start to finish!" - R. Lux</p>
+                <p>"Beast of a brand! Will do business with for a long time!" - C. Santana</p>
             </div>
         );
     }
